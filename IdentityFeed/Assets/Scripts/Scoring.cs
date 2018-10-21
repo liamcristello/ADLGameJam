e144d20188e2cdd71a67b4daef94435a41d15122
book81able <ecobb81@gmail.com>
2018-10-21 10:54:06 -0400

    Scoring Script

--- IdentityFeed/Assets/Scripts/Scoring.cs
+++ IdentityFeed/Assets/Scripts/Scoring.cs
@@ -8,13 +8,17 @@ public class Scoring : MonoBehaviour {
     public int score;
     public TextAsset topics;
     public Text contentArea;
-    private string topic;
+    private float multiplier;
+    private float percent;
+    public string topic;
+    private bool topicSeen;
     private char lineSep = '\n';
     private char rowSep = ',';
 	// Use this for initialization
 	void Start () {
-        topic = "cats";
         readData();
+        multiplier = percent;
+        addFollowers();
 	}
 	
 	// Update is called once per frame
@@ -22,16 +26,42 @@ public class Scoring : MonoBehaviour {
         
     }
 
+    private void addFollowers(){
+        Debug.Log(multiplier);
+        if (multiplier > 0 && multiplier < .33)
+        {
+            multiplier *= 15;
+            multiplier -= 5;
+        }else if(multiplier >= .33 && multiplier<.5){
+            multiplier *= 6;
+            multiplier -= 2;
+        }else if(multiplier >= .5 && multiplier<=1){
+            multiplier *= 8;
+            multiplier -= 3;
+        }else{
+            multiplier = 1;
+        }
+        multiplier *= 100;
+        multiplier = Mathf.Ceil(multiplier);
+        contentArea.text += multiplier;
+    }
+
     private void readData(){
         string[] records = topics.text.Split(lineSep);
-        foreach (string record in records)
-        {
+        foreach (string record in records) {
             string[] fields = record.Split(rowSep);
-            foreach (string field in fields)
-            {
-                contentArea.text += field + "\t";
+            foreach (string field in fields) {
+                if (topicSeen)
+                {
+                    percent = float.Parse(field);
+                }
+                if(field == topic){
+                    topicSeen = true;
+
+                }else{
+                    topicSeen = false;
+                }
             }
-            contentArea.text += '\n';
         }
     }
 }
