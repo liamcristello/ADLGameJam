1da661b4551e46124d17d6c591ad2ce764859ed0
Liam Cristello <liamcristello@gmail.com>
2018-10-20 21:36:34 -0400

    Establishing keywords

---
+++ IdentityFeed/Assets/Scripts/PostInit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostInit : MonoBehaviour {
+    // Every post has a percentage of people that agree with that sentiment
+    public int percentAgree;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
--- IdentityFeed/Assets/Scripts/Scoring.cs
+++ IdentityFeed/Assets/Scripts/Scoring.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Scoring : MonoBehaviour {
-    public int score;
+    // The amount of followers
+    public int followers;
+    // What turn is it
+    public int turn;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
